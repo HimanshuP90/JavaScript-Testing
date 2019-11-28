7b73ab4760f5999017bb587e035eb88e51a12b74
HimanshuP90 <himanshup895@gmail.com>
2019-11-28 17:08:00 +0530

    unit, integration and e2e added

--- util.test.js
+++ util.test.js
@@ -1,25 +1,28 @@
 const puppeteer = require('puppeteer');
 const { generateText, checkAndGenerate } = require('./util');
 
+// simple unit testing
 test('should output name and age', () => {
   const text = generateText('Himanshu', 28);
   expect(text).toBe('Himanshu (28 years old)');
 });
 
+// integration testing
 test('should generate a valid text output', () => {
   const text = checkAndGenerate('Himanshu', 28);
   expect(text).toBe('Himanshu (28 years old)');
 });
 
+// e2e testing
 test('should create an element with text and correct class', async () => {
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
     slowMo: 80,
     args: ['--window-size=1920,1080']
   });
   const page = await browser.newPage();
   await page.goto(
-    'file:///Users/himanshukumarpandey/Documents/JavaScript-Testing/index.html'
+    'file:///home/himanshu/Documents/JavaScript-Testing/index.html'
   );
   await page.click('input#name');
   await page.type('input#name', 'Himanshu');
@@ -28,4 +31,4 @@ test('should create an element with text and correct class', async () => {
   await page.click('#btnAddUser');
   const finalText = await page.$eval('.user-item', el => el.textContent);
   expect(finalText).toBe('Himanshu (27 years old)');
-}, 10000);
+}, 50000);
